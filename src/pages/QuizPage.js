2da4612d466bb16fe97437e99c6ade57ab0b4be8
vberestyukova <berestyukova.vm@gmail.com>
2022-05-05 02:11:50 +0300

    Получила массивы с вопросами, ответами и неверными ответами

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -49,18 +49,18 @@ export class QuizPage extends Component{
 
         const quiz = localStorage.getItem('json');
         const quizInfo = JSON.parse(quiz).results;
-        // console.log(quizInfo);
+
         const questionCase = [];
         const correct_answerCase = [];
         const incorrect_answersCase = [];
-        // console.log(quizInfo);
-        // console.log(quizInfo.results.correct_answer);
         for (let i = 0; i < 10; i++) {
-            // questionCase.push((quizInfo[i]).question);
-            // correct_answerCase.push((quizInfo.results[i]).correct_answer);
-            // incorrect_answersCase.push((quizInfo.results[i]).incorrect_answers);
+            questionCase.push((quizInfo[i]).question);
+            correct_answerCase.push((quizInfo[i]).correct_answer);
+            incorrect_answersCase.push((quizInfo[i]).incorrect_answers);
         }
-
+        console.log(correct_answerCase);
+        console.log(correct_answerCase);
+        console.log(incorrect_answersCase);
         return (
             <div className='page_settings'>
                 <div className='quiz'>
